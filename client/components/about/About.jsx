760f1c4945ca949d020691048b26a438f9a17dbf
andela-tisrael <israel.tomilayo@andela.com>
2017-02-13 10:30:41 +0100

    feature: setup webpack and testing enviroment

---
+++ client/components/about/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class AboutPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>About Document Management</h1>
+        <p>React front end rendering of about page</p>
+      </div>
+    );
+  }
+}
+export default AboutPage;
